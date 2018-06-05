416152a913f7ddfb3844535c6b91b848031abac1
Revansiddh Patil <rpatil1@DSRM.local>
2018-06-05 17:44:17 +0530

    new compnnent

--- src/common/components/index.js
+++ src/common/components/index.js
@@ -1,4 +1,7 @@
 import Header from './header';
 import Sidebar from './sidebar';
+import MainContainer from './mainContainer';
+import PageHeader from './pageHeader';
+import Table from './table/table';
 
-export { Header, Sidebar }
+export { Header, Sidebar, MainContainer, PageHeader, Table }
