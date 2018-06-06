7d4dbbfc15332d88afec22197e46942c65dadea7
Revansiddh Patil <rpatil1@DSRM.local>
2018-06-06 12:07:13 +0530

    sidebar optimization

--- src/common/components/sidebar.js
+++ src/common/components/sidebar.js
@@ -1,27 +1,40 @@
 import React from 'react';
 import { Link, withRouter} from 'react-router-dom';
+import { STORAGE_KEYS } from '../constants/stringConstant';
+
+
+const item = [
+    { name: 'Dashboard', icon: 'menu-icon fa fa-tachometer', path: "app" },
+    { name: 'Users', icon: 'menu-icon fa fa-user-plus', path: "/app/users" },
+    { name: 'Groups', icon: 'menu-icon fa fa-users', path: "/app/groups" },
+]
 const Sidebar = () => {
+    const user = JSON.parse(localStorage.getItem(STORAGE_KEYS.USER_DETAILS));
+    const adminItems = user && user.role === 'admin' || user && user.role === 'super'
+                     ? item.map((m)=>{
+                            return <SidebarItem name={m.name} icon={m.icon} path={m.path} />
+                        }) 
+                     : null
     return (
     <div id="sidebar" className="sidebar responsive ace-save-state" data-sidebar="true" data-sidebar-scroll="true" data-sidebar-hover="true">
         <ul className="nav nav-list" style={{top: 0}}>
-            <li className="active">
-                <Link to="/">
-                    <i className="menu-icon fa fa-tachometer" />
-                    <span className="menu-text"> Dashboard </span>
-                </Link>
-                <b className="arrow" />
-            </li>
-            <li className>
-                <Link to="/users">
-                    <i className="menu-icon fa fa-picture-o" />
-                    <span className="menu-text"> Users </span>
-                </Link>
-                <b className="arrow" />
-            </li>
+            { adminItems }
+            <SidebarItem name='Features' icon='menu-icon fa fa-code' path="/app/features"/>,
         </ul>
     </div>
 
     );
 };
-
+const SidebarItem = (props) =>{
+    return(
+        <li className>
+                <Link to={`${props.path}`}>
+                    <i className={`menu-icon ${props.icon}`} />
+                    <span className="menu-text"> {props.name} </span>
+                </Link>
+                <b className="arrow" />
+        </li>
+    )
+}
 export default withRouter(Sidebar);
+// fa fa-file-code-o
